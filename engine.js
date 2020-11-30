edd45abed352fa39035192762533dae8e140adcb
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2020-11-30 18:32:29 +1100

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,5 +1,4 @@
 /*
-
 ************************************************************************
 *                                                                      *
 * (Use full screen.)                                                   *
@@ -19,7 +18,6 @@
 *  The following code runs a remake of the classic game snake.         *
 *                                                                      *
 ************************************************************************
-
 */
 
 // Bind keys
@@ -29,7 +27,7 @@ function bindKeys() {
 		if (keyPause === 0) {
 			started = true;
 			switch (e.keyCode) {
-					// WASD
+				// WASD
 				case 87:
 					if (dir != 2) {
 						dir = 1;
@@ -54,7 +52,6 @@ function bindKeys() {
 					}
 					keyPause = 1;
 					break;
-
 					// ARROWS
 				case 38:
 					if (dir != 2) {
@@ -80,7 +77,6 @@ function bindKeys() {
 					}
 					keyPause = 1;
 					break;
-
 					// RESTART
 				case 32:
 					dir = 0;
@@ -90,19 +86,20 @@ function bindKeys() {
 					ay = 200;
 					score = 0;
 					exit = false;
-					tail = [[0], [0]];
+					tail = [
+						[0],
+						[0]
+					];
 					keyPause = 0;
 					break;
 			}
 		}
 	}
 }
-
 // Draw Frame
 function drawFrame() {
 	canvCtx.beginPath();
-	canvCtx.clearRect(0,0, canv.width, canv.height);
-	
+	canvCtx.clearRect(0, 0, canv.width, canv.height);
 	// Render snake
 	canvCtx.fillStyle = "darkgreen";
 	canvCtx.fillRect(x + 1, y + 1, 24, 24);
@@ -110,18 +107,16 @@ function drawFrame() {
 		canvCtx.fillStyle = "green";
 		canvCtx.fillRect(tail[0][i] + 1, tail[1][i] + 1, 24, 24);
 	}
-	
 	// Render apple
 	canvCtx.fillStyle = "red";
 	canvCtx.fillRect(ax, ay, 25, 25);
 	canvCtx.closePath;
 	canvCtx.fill;
-	
 	// Render score or "game over" screen
 	if (exit === false) {
 		canvCtx.fillStyle = "black";
 		canvCtx.font = "20px Arial";
-		canvCtx.fillText("Score: " + score, 10, 25);
+		canvCtx.fillText("Score: " + score, 5, 20);
 		if (started === false) {
 			canvCtx.fillStyle = "black";
 			canvCtx.font = "20px Arial";
@@ -136,11 +131,10 @@ function drawFrame() {
 		canvCtx.fillText("Press space to play again.", canv.width / 7, canv.height - 200);
 	}
 }
-
 // Update positioning variables and sense if off screen
 function positioning() {
 	if (exit === false) {
-		// Tail
+		// Move tail
 		tail[0][0] = x;
 		tail[1][0] = y;
 		if (score > 0) {
@@ -149,7 +143,6 @@ function positioning() {
 				tail[1][i] = tail[1][i - 1];
 			}
 		}
-		
 		// Head
 		if (dir === 1) {
 			y -= 25;
@@ -160,10 +153,8 @@ function positioning() {
 		} else if (dir === 4) {
 			x += 25;
 		}
-		
 		// Allow player to change direction again
 		keyPause = 0;
-		
 		// Apple
 		if (x === ax) {
 			if (y === ay) {
@@ -184,7 +175,6 @@ function positioning() {
 		} else if (y > 400) {
 			exit = true;
 		}
-		
 		// Touching tail
 		for (let i = score; i > 0; i--) {
 			if (i > 3) {
@@ -204,7 +194,6 @@ function tick() {
 	}
 	drawFrame();
 }
-
 //Initialise
 let keyPause = 0;
 bindKeys();
@@ -218,9 +207,11 @@ let ay = 200;
 let score = 0;
 let exit = false;
 let pause = false;
-let tail = [[0], [0]];
+let tail = [
+	[0],
+	[0]
+];
 let started = false;
-
 //Game loop
 let gameloop = setInterval(function() {
 	tick();
